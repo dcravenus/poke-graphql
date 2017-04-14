1277dd7d5c6bf090898d482a25c6795f62fbbf9f
Derrick Scott Craven <dcravenus@gmail.com>
2017-04-14 15:39:23 -0600

    Hello world example

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {hello: ()=> 'Hello world!'};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(4000, () => console.log('Server running'));
